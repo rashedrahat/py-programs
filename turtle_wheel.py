b1d3a948247c6bb315266e5af3b01c1f38cf4a34
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-02-28 14:23:45 +0600

    Update turtle_wheel.py

--- turtle_wheel.py
+++ turtle_wheel.py
@@ -5,11 +5,11 @@ turtle.speed(5)
 turtle.shape("turtle")
 turtle.color("red")
 
-c = 0
-while c < 36:
+counter = 0
+while counter < 36:
     for i in range(4):
         turtle.forward(100)
         turtle.left(90)
     turtle.right(10)
-    c += 1
+    counter += 1
 turtle.exitonclick()
