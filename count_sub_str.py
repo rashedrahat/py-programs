0fd57a8ff6a6f8e4ce9cee5c2de57cbc5ff4ba14
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-03-06 13:58:02 +0600

    Add files via upload

---
+++ count_sub_str.py
@@ -0,0 +1,6 @@
+# a program of counting a sub string how many times contains in a string.
+
+str = "a quick brown fox jumps over the lazy dog"
+
+for c in "abcdefghijklmnopqrstuvwxyz":
+	print(c, str.count(c))
