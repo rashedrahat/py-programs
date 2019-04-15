61b8d6ea49115f3f98a93f9c6a3c13e25b60307b
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-04-15 17:13:32 +0600

    Add files via upload

---
+++ web_page_file.py
@@ -0,0 +1,12 @@
+# a program that saves a web page into a file and opens the file.
+
+import requests
+import os
+import webbrowser as wb
+
+url = "https://rashedrahat.github.io/imRAR"
+res = requests.get(url)
+with open("ProfileOfRashedRahat.html", "w", encoding = res.encoding) as f:
+    f.write(res.text)
+file_path = os.path.realpath("ProfileOfRashedRahat.html")
+wb.open("file://" + file_path)
