460a31239a4141d1a0c9c25c5ce9a53cd775b188
Rashed Ahmed Rahat <37572137+rashedrahat@users.noreply.github.com>
2019-04-15 17:14:19 +0600

    Update web_page_file.py

--- web_page_file.py
+++ web_page_file.py
@@ -6,7 +6,9 @@ import webbrowser as wb
 
 url = "https://rashedrahat.github.io/imRAR"
 res = requests.get(url)
+
 with open("ProfileOfRashedRahat.html", "w", encoding = res.encoding) as f:
     f.write(res.text)
+
 file_path = os.path.realpath("ProfileOfRashedRahat.html")
 wb.open("file://" + file_path)
